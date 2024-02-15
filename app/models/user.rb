e28ddba6703b54a7351b159063584a2576b0d384
JoaoJorgeEF <joaojorgeevangelista@gmail.com>
2024-02-15 14:58:40 -0300

    add following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   # A user can follow many other users
   has_many :followings, foreign_key: :follower_id
-  has_many :followed_users, through: :followings, source: :followed
+  has_many :follows, through: :followings, source: :followed
 
   # A user can be followed by many other users
   has_many :inverse_followings, class_name: 'Following', foreign_key: :followed_id

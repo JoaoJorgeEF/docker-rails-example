59d96716808e2b94f5f30317cec8266cdd61c351
JoaoJorgeEF <joaojorgeevangelista@gmail.com>
2024-02-15 15:06:34 -0300

    chat scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_233712) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_175859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.bigint "following_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["following_id"], name: "index_chats_on_following_id"
+  end
 
   create_table "followings", force: :cascade do |t|
     t.integer "follower_id"
@@ -34,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_233712) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "followings"
 end
